4fb1a1e1bae0c5845fa2d1a140b7eb1c6869845e
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2023-11-15 11:52:21 -0500

    output check

--- BIDS/BIDS_heudiconv/BIDSify_7T.sh
+++ BIDS/BIDS_heudiconv/BIDSify_7T.sh
@@ -11,7 +11,7 @@ cd $input_dicomdir
 for sub in 1* ; do
 	subject=${sub%_*}
 	session=${sub#*_}
-	if ! [ -d $bids_dir/$subject/$session ] ; then
+	if ! [ -d $bids_dir/sub-$subject/ses-$session ] ; then
 	heudiconv -d $bidscompliant_dicomdir/{subject}/{session}/*/* -s $subject -ss $session -f /Volumes/Hera/Projects/corticalmyelin_development/code/corticalmyelin_maturation/BIDS/BIDS_heudiconv/7TBrainMech_MP2RAGE_heuristic.py -c dcm2niix -b -o $bids_dir --grouping custom &> $logs_dir/${subject}_${session}-log.txt
 	fi
 done
