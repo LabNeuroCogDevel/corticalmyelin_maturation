ee1b3f409b953fb55cabbfe1aea1e0692184c11f
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2023-12-08 14:00:47 -0500

    organize unicort output

---
+++ B1+_transmitfield_correction/unicort/organize_unicort_files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#A script to organize the output of SPM's UNICORT run on UNI images, including copying the inhomogeneity corrected UNI image to BIDS/ for freesurfer and deleting redundant files the software copies from BIDS/ to output/
+
+BIDS_dir=/Volumes/Hera/Projects/corticalmyelin_development/BIDS
+unicort_UNIdir=/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/unicort_UNI
+participant_file=/Volumes/Hera/Projects/corticalmyelin_development/sample_info/7T_CuBIDS_subses_list.txt 
+
+while read line; do
+	subses=$line
+	subject_id=${subses% *}
+	session_id=${subses#* }
+	#Bring the corrected UNI image to BIDS_dir for further analysis with BIDS-apps
+	echo "copying over corrected UNI to BIDS"
+	cp ${unicort_UNIdir}/${subject_id}/${session_id}/anat/m${subject_id}_${session_id}_acq-UNIDENT1_T1w.nii ${BIDS_dir}/${subject_id}/${session_id}/anat/${subject_id}_${session_id}_acq-UNIDENT1corrected_T1w.nii
+	echo "gzipping"
+	gzip ${BIDS_dir}/${subject_id}/${session_id}/anat/${subject_id}_${session_id}_acq-UNIDENT1corrected_T1w.nii
+	#Make sure there is a matching json file
+	cp ${BIDS_dir}/${subject_id}/${session_id}/anat/${subject_id}_${session_id}_acq-UNIDENT1_T1w.json ${BIDS_dir}/${subject_id}/${session_id}/anat/${subject_id}_${session_id}_acq-UNIDENT1corrected_T1w.json  	
+        #Clean up unnecesary files
+	rm ${unicort_UNIdir}/${subject_id}/${session_id}/anat/sub*nii 
+        rm ${unicort_UNIdir}/${subject_id}/${session_id}/anat/sub*json
+done < ${participant_file}	
+
