9cf205755ae96ae2b21fbbbb7236f298b0436eb9
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2024-03-08 10:28:59 -0500

    R1 and anat regional measures

---
+++ surface_metrics/surface_measures/collate_study_anatomicalstats.py
@@ -0,0 +1,23 @@
+#A script to create a df with all participants' surface metrics for all atlases and specific atlases of interest 
+
+from pathlib import Path
+import pandas as pd
+
+print("Creating a surfstats parquet for all atlases")
+dataframes = []
+for tsv in Path('/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics').rglob('*regionsurfacestats.tsv'):
+    dataframes.append(pd.read_csv(tsv, sep="\t"))
+group_surfacestats = pd.concat(dataframes, ignore_index=True, axis=0)
+group_surfacestats.to_parquet('/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/7T_BrainMechanisms_surfacestats_allatlases.parquet')
+
+atlases = ['aparc','glasser','gordon333dil', 'Juelich', 'Schaefer2018_400Parcels_17Networks_order', 'Schaefer2018_200Parcels_17Networks_order']
+
+for this_atlas in atlases:
+    print("Creating a surfstats parquet for " + this_atlas)
+    atlas_dfs = []
+    for tsv in Path('/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics').rglob('*regionsurfacestats.tsv'):
+        _this_df = pd.read_csv(tsv, sep="\t")
+        atlas_dfs.append(_this_df[_this_df["atlas"] == this_atlas])
+    atlas_df = pd.concat(atlas_dfs, ignore_index=True, axis=0)
+    atlas_df.to_parquet('/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/7T_BrainMechanisms_surfacestats_{0}.parquet'.format(this_atlas))
+
