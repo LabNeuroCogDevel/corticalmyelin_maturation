4a4a040466e3ea2dc7fc41ad940483bddf2890f6
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2024-03-26 12:17:33 -0400

    create whole-brain measure parquet

--- surface_metrics/surface_measures/collate_study_anatomicalstats.py
+++ surface_metrics/surface_measures/collate_study_anatomicalstats.py
@@ -21,3 +21,9 @@ for this_atlas in atlases:
     atlas_df = pd.concat(atlas_dfs, ignore_index=True, axis=0)
     atlas_df.to_parquet('/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/7T_BrainMechanisms_surfacestats_{0}.parquet'.format(this_atlas))
 
+print('Creating whole-brain measures parquet')
+dataframes = []
+for tsv in Path('/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics').rglob('*brainmeasures.tsv'):
+    dataframes.append(pd.read_csv(tsv, sep="\t"))
+group_brainstats = pd.concat(dataframes, ignore_index=True, axis=0)
+group_brainstats.to_parquet('/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/7T_BrainMechanisms_brainmeasures.parquet')
