256d21f3c4c5622e7aaced588226d722ae47af73
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2024-03-27 18:18:56 -0400

    templateflow volume & surface templates

---
+++ software_installation/templateflow_templates.sh
@@ -0,0 +1,5 @@
+export TEMPLATEFLOW_HOME=/Volumes/Hera/Projects/corticalmyelin_development/Maps/Templates/
+python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="164k",suffix="white",desc=None, extension="surf.gii"))'
+python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="164k",suffix="midthickness",desc=None, extension="surf.gii"))'
+python3 -c 'import templateflow.api as tf; print(tf.get("MNI152NLin2009cAsym", resolution=[1,2],desc=None, suffix="T1w"))'
+
