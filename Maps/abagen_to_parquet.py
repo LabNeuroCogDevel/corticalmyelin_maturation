7b824e62d20fc2ea6670a6844ac5481878e86b01
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2024-05-28 13:51:23 -0400

    repo reorganization, RR

--- Maps/abagen_to_parquet.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('/Volumes/Hera/Projects/corticalmyelin_development/Maps/AHBA/AHBA_geneexpression_glasser.csv')
-df.to_parquet('/Volumes/Hera/Projects/corticalmyelin_development/Maps/AHBA/AHBA_geneexpression_glasser.parquet')
