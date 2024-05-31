22874d488869e29973bac42b6e50ebbc510c9b97
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2024-05-31 11:49:31 -0400

    gimme,gimme,gimme (templates/a man after midnight)

--- software_installation/templateflow_templates.sh
+++ software_installation/templateflow_templates.sh
@@ -2,5 +2,6 @@ export TEMPLATEFLOW_HOME=/Volumes/Hera/Projects/corticalmyelin_development/Maps/
 python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="164k",suffix="white",desc=None, extension="surf.gii"))'
 python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="164k",suffix="midthickness",desc=None, extension="surf.gii"))'
 python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="164k",suffix="pial",desc=None, extension="surf.gii"))'
+python3 -c 'import templateflow.api as tf; print(tf.get("fsLR", density="32k",suffix="midthickness",desc=None, extension="surf.gii"))'
 python3 -c 'import templateflow.api as tf; print(tf.get("MNI152NLin2009cAsym", resolution=[1,2],desc=None, suffix="T1w"))'
 
