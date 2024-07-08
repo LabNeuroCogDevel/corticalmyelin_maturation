849767575311fce0fc63b1a782c430fcdfaddd9d
Valerie Jill Sydnor <valeriejill.sydnor@gmail.com>
2024-07-08 15:35:52 -0400

    gimme,gimme,gimme (templates/a man after midnight)

--- software_installation/templateflow_templates.sh
+++ software_installation/templateflow_templates.sh
@@ -3,5 +3,6 @@ python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="16
 python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="164k",suffix="midthickness",desc=None, extension="surf.gii"))'
 python3 -c 'import templateflow.api as tf; print(tf.get("fsaverage", density="164k",suffix="pial",desc=None, extension="surf.gii"))'
 python3 -c 'import templateflow.api as tf; print(tf.get("fsLR", density="32k",suffix="midthickness",desc=None, extension="surf.gii"))'
+python3 -c 'import templateflow.api as tf; print(tf.get("fsLR", density="164k",suffix="midthickness",desc=None, extension="surf.gii"))'
 python3 -c 'import templateflow.api as tf; print(tf.get("MNI152NLin2009cAsym", resolution=[1,2],desc=None, suffix="T1w"))'
 
