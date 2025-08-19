a47acecd18f9a8d5957e2d12681051fc53be99fa
Valerie Sydnor <valeriesydnor@Valeries-MacBook-Pro.local>
2025-08-19 11:49:58 -0400

    superficial, middle, deep R1

--- surface_metrics/surface_measures/extract_depthdependent_R1.R
+++ surface_metrics/surface_measures/extract_depthdependent_R1.R
@@ -10,23 +10,23 @@ source("/Volumes/Hera/Projects/corticalmyelin_development/code/corticalmyelin_ma
 participants <- read.csv("/Volumes/Hera/Projects/corticalmyelin_development/sample_info/7T_MP2RAGE_finalsample_demographics.csv") #created by /sample_construction/finalsample_7Tmyelin.Rmd
 participants <- participants %>% mutate(subses = sprintf("%s_%s", subject_id, session_id)) #create a unique scan identifier 
   
-# Myelin measures
-mean.myelin.measures <- list("Mean_R1map.0.8%","Mean_R1map.0.7%","Mean_R1map.0.6%","Mean_R1map.0.5%","Mean_R1map.0.4%","Mean_R1map.0.3%","Mean_R1map.0.2%") #measures to extract data for 
+# R1 within the cortical ribbon (10% increments)
+depth.R1.measures <- list("Mean_R1map.1.00%", "Mean_R1map.0.9%", "Mean_R1map.0.8%","Mean_R1map.0.7%","Mean_R1map.0.6%","Mean_R1map.0.5%","Mean_R1map.0.4%","Mean_R1map.0.3%","Mean_R1map.0.2%", "Mean_R1map.0.1%", "Mean_R1map.0.0%") #R1 at all cortical depths
 
 # Glasser (HCP-MMP) atlas
-myelin.glasser.7T <- lapply(mean.myelin.measures, function(x) {
+myelin.glasser.7T <- lapply(depth.R1.measures, function(x) {
   extract_surfacestats("glasser", x)}) #glasser atlas please
-names(myelin.glasser.7T) <- list("depth_1", "depth_2", "depth_3", "depth_4", "depth_5", "depth_6", "depth_7")
+names(myelin.glasser.7T) <- list("depth_100%", "depth_90%", "depth_80%", "depth_70%", "depth_60%", "depth_50%", "depth_40%", "depth_30%", "depth_20%", "depth_10%", "depth_0%")
 saveRDS(myelin.glasser.7T, "/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/depthR1_glasseratlas_finalsample.RDS")
 
 # EEG electrode atlas
-myelin.electrodes.7T <- lapply(mean.myelin.measures, function(x) {
+myelin.electrodes.7T <- lapply(depth.R1.measures, function(x) {
   extract_surfacestats("EEGatlas", x)}) #EEG electrode regions please
-names(myelin.electrodes.7T) <- list("depth_1", "depth_2", "depth_3", "depth_4", "depth_5", "depth_6", "depth_7")
+names(myelin.electrodes.7T) <- list("depth_100%", "depth_90%", "depth_80%", "depth_70%", "depth_60%", "depth_50%", "depth_40%", "depth_30%", "depth_20%", "depth_10%", "depth_0%")
 saveRDS(myelin.electrodes.7T, "/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/depthR1_EEGatlas_finalsample.RDS")
 
 ############################################################################################################
-#### Calculate Superficial and Deep R1 ####
+#### Superficial, Middle, and Deep Cortex R1 ####
 
 # Glasser (HCP-MMP) atlas
 myelin.glasser.7T <- lapply(myelin.glasser.7T, function(depth){
@@ -35,30 +35,41 @@ myelin.glasser.7T <- lapply(myelin.glasser.7T, function(depth){
   return(depth)})
 
 ### superficial depths
-SGmyelin.glasser.7T <- do.call(rbind, myelin.glasser.7T[1:3]) #merge data for top 3 depths
-SGmyelin.glasser.7T$depth <- substr(row.names(SGmyelin.glasser.7T), 1, 7) #assign depth
-cols_to_pivot <- names(SGmyelin.glasser.7T)[grep("ROI", names(SGmyelin.glasser.7T))] #atlas region cols
-SGmyelin.glasser.7T.long <- SGmyelin.glasser.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
-SGmyelin.glasser.7T <- SGmyelin.glasser.7T.long %>% group_by(subses, region) %>% #mean R1 in depths 1-3 for each region
+superficialR1.glasser.7T <- do.call(rbind, myelin.glasser.7T[3:4]) #20-30% of cortical thickness
+superficialR1.glasser.7T$depth <- substr(row.names(superficialR1.glasser.7T), 1, 9) #assign depth
+cols_to_pivot <- names(superficialR1.glasser.7T)[grep("ROI", names(superficialR1.glasser.7T))] #atlas region cols
+superficialR1.glasser.7T.long <- superficialR1.glasser.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
+superficialR1.glasser.7T <- superficialR1.glasser.7T.long %>% group_by(subses, region) %>% 
                         do(superficial_R1 = mean(.$R1)) %>% 
                         unnest(cols = superficial_R1) %>% 
                         pivot_wider(id_cols = subses, names_from = "region", values_from = "superficial_R1")
-SGmyelin.glasser.7T <- merge(SGmyelin.glasser.7T, participants, by = "subses")
+superficialR1.glasser.7T <- merge(superficialR1.glasser.7T, participants, by = "subses")
+
+### middle depths 
+middleR1.glasser.7T <- do.call(rbind, myelin.glasser.7T[5:7]) #40-60% of cortical thickness
+middleR1.glasser.7T$depth <- substr(row.names(middleR1.glasser.7T), 1, 9) 
+cols_to_pivot <- names(middleR1.glasser.7T)[grep("ROI", names(middleR1.glasser.7T))] 
+middleR1.glasser.7T.long <- middleR1.glasser.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
+middleR1.glasser.7T <- middleR1.glasser.7T.long %>% group_by(subses, region) %>% 
+  do(middle_R1 = mean(.$R1)) %>% 
+  unnest(cols = middle_R1) %>% 
+  pivot_wider(id_cols = subses, names_from = "region", values_from = "middle_R1")
+middleR1.glasser.7T <- merge(middleR1.glasser.7T, participants, by = "subses")
 
 ### deep depths
-IGmyelin.glasser.7T <- do.call(rbind, myelin.glasser.7T[4:7]) #merge data for bottom 4 depths
-IGmyelin.glasser.7T$depth <- substr(row.names(IGmyelin.glasser.7T), 1, 7) 
-cols_to_pivot <- names(IGmyelin.glasser.7T)[grep("ROI", names(IGmyelin.glasser.7T))] 
-IGmyelin.glasser.7T.long <- IGmyelin.glasser.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") 
-IGmyelin.glasser.7T <- IGmyelin.glasser.7T.long %>% group_by(subses, region) %>% #mean R1 in depths 4-7 for each region
-                        do(deep_R1 = mean(.$R1)) %>% 
-                        unnest(cols = deep_R1) %>% 
-                        pivot_wider(id_cols = subses, names_from = "region", values_from = "deep_R1")
-IGmyelin.glasser.7T <- merge(IGmyelin.glasser.7T, participants, by = "subses")
+deepR1.glasser.7T <- do.call(rbind, myelin.glasser.7T[8:9]) #70-80% of cortical thickness
+deepR1.glasser.7T$depth <- substr(row.names(deepR1.glasser.7T), 1, 9) 
+cols_to_pivot <- names(deepR1.glasser.7T)[grep("ROI", names(deepR1.glasser.7T))] 
+deepR1.glasser.7T.long <- deepR1.glasser.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
+deepR1.glasser.7T <- deepR1.glasser.7T.long %>% group_by(subses, region) %>% 
+  do(deep_R1 = mean(.$R1)) %>% 
+  unnest(cols = deep_R1) %>% 
+  pivot_wider(id_cols = subses, names_from = "region", values_from = "deep_R1")
+deepR1.glasser.7T <- merge(deepR1.glasser.7T, participants, by = "subses")
 
-SGIGmyelin.glasser.7T <- list(SGmyelin.glasser.7T, IGmyelin.glasser.7T)
-names(SGIGmyelin.glasser.7T) <- list("superficial", "deep")
-saveRDS(SGIGmyelin.glasser.7T, "/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/SGIGR1_glasseratlas_finalsample.RDS")
+compartments.myelin.glasser.7T <- list(superficialR1.glasser.7T, middleR1.glasser.7T, deepR1.glasser.7T)
+names(compartments.myelin.glasser.7T) <- list("superficial", "middle", "deep")
+saveRDS(compartments.myelin.glasser.7T, "/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/compartmentsR1_glasseratlas_finalsample.RDS")
 
 # EEG electrode atlas
 myelin.electrodes.7T <- lapply(myelin.electrodes.7T, function(depth){
@@ -67,28 +78,38 @@ myelin.electrodes.7T <- lapply(myelin.electrodes.7T, function(depth){
   return(depth)})
 
 ### superficial depths
-SGmyelin.electrodes.7T <- do.call(rbind, myelin.electrodes.7T[1:3]) #merge data for top 3 depths
-SGmyelin.electrodes.7T$depth <- substr(row.names(SGmyelin.electrodes.7T), 1, 7) #assign depth
-cols_to_pivot <- names(SGmyelin.electrodes.7T)[16:69] #atlas region cols
-SGmyelin.electrodes.7T.long <- SGmyelin.electrodes.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
-SGmyelin.electrodes.7T <- SGmyelin.electrodes.7T.long %>% group_by(subses, region) %>% #mean R1 in depths 1-3 for each region
+superficialR1.electrodes.7T <- do.call(rbind, myelin.electrodes.7T[3:4]) 
+superficialR1.electrodes.7T$depth <- substr(row.names(superficialR1.electrodes.7T), 1, 9) #assign depth
+cols_to_pivot <- names(superficialR1.electrodes.7T)[16:69] #atlas region cols
+superficialR1.electrodes.7T.long <- superficialR1.electrodes.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
+superficialR1.electrodes.7T <- superficialR1.electrodes.7T.long %>% group_by(subses, region) %>% 
   do(superficial_R1 = mean(.$R1)) %>% 
   unnest(cols = superficial_R1) %>% 
   pivot_wider(id_cols = subses, names_from = "region", values_from = "superficial_R1")
-SGmyelin.electrodes.7T <- merge(SGmyelin.electrodes.7T, participants, by = "subses")
+superficialR1.electrodes.7T <- merge(superficialR1.electrodes.7T, participants, by = "subses")
+
+### middle depths
+middleR1.electrodes.7T <- do.call(rbind, myelin.electrodes.7T[5:7]) 
+middleR1.electrodes.7T$depth <- substr(row.names(middleR1.electrodes.7T), 1, 9) #assign depth
+cols_to_pivot <- names(middleR1.electrodes.7T)[16:69] #atlas region cols
+middleR1.electrodes.7T.long <- middleR1.electrodes.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
+middleR1.electrodes.7T <- middleR1.electrodes.7T.long %>% group_by(subses, region) %>% 
+  do(middle_R1 = mean(.$R1)) %>% 
+  unnest(cols = middle_R1) %>% 
+  pivot_wider(id_cols = subses, names_from = "region", values_from = "middle_R1")
+middleR1.electrodes.7T <- merge(middleR1.electrodes.7T, participants, by = "subses")
 
 ### deep depths
-IGmyelin.electrodes.7T <- do.call(rbind, myelin.electrodes.7T[4:7]) #merge data for bottom 4 depths
-IGmyelin.electrodes.7T$depth <- substr(row.names(IGmyelin.electrodes.7T), 1, 7) #assign depth
-cols_to_pivot <- names(IGmyelin.electrodes.7T)[16:69] #atlas region cols
-IGmyelin.electrodes.7T.long <- IGmyelin.electrodes.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") 
-IGmyelin.electrodes.7T <- IGmyelin.electrodes.7T.long %>% group_by(subses, region) %>%
+deepR1.electrodes.7T <- do.call(rbind, myelin.electrodes.7T[8:9]) 
+deepR1.electrodes.7T$depth <- substr(row.names(deepR1.electrodes.7T), 1, 9) #assign depth
+cols_to_pivot <- names(deepR1.electrodes.7T)[16:69] #atlas region cols
+deepR1.electrodes.7T.long <- deepR1.electrodes.7T %>% pivot_longer(cols = all_of(cols_to_pivot), names_to = "region", values_to = "R1") #long formatted df for grouping
+deepR1.electrodes.7T <- deepR1.electrodes.7T.long %>% group_by(subses, region) %>% 
   do(deep_R1 = mean(.$R1)) %>% 
   unnest(cols = deep_R1) %>% 
   pivot_wider(id_cols = subses, names_from = "region", values_from = "deep_R1")
-IGmyelin.electrodes.7T <- merge(IGmyelin.electrodes.7T, participants, by = "subses")
+deepR1.electrodes.7T <- merge(deepR1.electrodes.7T, participants, by = "subses")
 
-SGIGmyelin.electrodes.7T <- list(SGmyelin.electrodes.7T, IGmyelin.electrodes.7T)
-names(SGIGmyelin.electrodes.7T) <- list("superficial", "deep")
-saveRDS(SGIGmyelin.electrodes.7T, "/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/SGIGR1_electrodeatlas_finalsample.RDS")
-
+compartments.myelin.electrodes.7T <- list(superficialR1.electrodes.7T, middleR1.electrodes.7T, deepR1.electrodes.7T)
+names(compartments.myelin.electrodes.7T) <- list("superficial", "middle", "deep")
+saveRDS(compartments.myelin.electrodes.7T, "/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/compartmentsR1_EEGatlas_finalsample.RDS")
