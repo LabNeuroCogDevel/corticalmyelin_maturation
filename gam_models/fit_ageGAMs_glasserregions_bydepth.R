39177f6077e9277fcf69350d8981002bcb81df29
Valerie Sydnor <valeriesydnor@Valeries-MacBook-Pro.local>
2025-10-05 15:19:48 -0400

    ribbon compartment R1 development

--- gam_models/fit_ageGAMs_glasserregions_bydepth.R
+++ gam_models/fit_ageGAMs_glasserregions_bydepth.R
@@ -11,21 +11,13 @@ library(dplyr)
 #Brain region list
 glasser.regions <- read.csv("/Volumes/Hera/Projects/corticalmyelin_development/Maps/HCPMMP_glasseratlas/glasser360_regionlist.csv")
 
-#Depth-specific R1 measures for final study sample
-myelin.glasser.7T <- readRDS("/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/depthR1_glasseratlas_finalsample.RDS")
+#Superficial, middle, and deep R1 for final study sample
+myelin.compartments.7T <- readRDS("/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/compartmentsR1_glasseratlas_finalsample.RDS")
 
-myelin.glasser.7T <- lapply(myelin.glasser.7T, function(depth){
+myelin.compartments.7T <- lapply(myelin.compartments.7T, function(depth){
   depth <- depth %>% mutate(subject_id = as.factor(subject_id)) #factor
   depth <- depth %>% mutate(sex = as.factor(sex)) #factor
   depth <- depth %>% mutate(osex = ordered(sex, levels = c("M", "F"))) #M <  F, obvi
-  return(depth)
-})
-
-#Superficial and deep R1 measures for final study sample
-SGIGmyelin.glasser.7T <- readRDS("/Volumes/Hera/Projects/corticalmyelin_development/BIDS/derivatives/surface_metrics/SGIGR1_glasseratlas_finalsample.RDS")
-
-SGIGmyelin.glasser.7T <- lapply(SGIGmyelin.glasser.7T, function(depth){
-  depth <- depth %>% mutate(subject_id = as.factor(subject_id)) #factor
   return(depth)
 })
 
@@ -72,16 +64,9 @@ run.gams.bydepth <- function(input.depth.df, output.df.name){
   gc(gam.outputs.statslist)
   }
 
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_1, output.df.name = "depth1_gamstatistics_age.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_2, output.df.name = "depth2_gamstatistics_age.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_3, output.df.name = "depth3_gamstatistics_age.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_4, output.df.name = "depth4_gamstatistics_age.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_5, output.df.name = "depth5_gamstatistics_age.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_6, output.df.name = "depth6_gamstatistics_age.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_7, output.df.name = "depth7_gamstatistics_age.RDS")
-
-run.gams.bydepth(input.depth.df = SGIGmyelin.glasser.7T$superficial, output.df.name = "superficialcortex_gamstatistics_age.RDS")
-run.gams.bydepth(input.depth.df = SGIGmyelin.glasser.7T$deep, output.df.name = "deepcortex_gamstatistics_age.RDS")
+run.gams.bydepth(input.depth.df = myelin.compartments.7T$superficial, output.df.name = "superficial_gamstatistics_age.RDS")
+run.gams.bydepth(input.depth.df = myelin.compartments.7T$middle, output.df.name = "middle_gamstatistics_age.RDS")
+run.gams.bydepth(input.depth.df = myelin.compartments.7T$deep, output.df.name = "deep_gamstatistics_age.RDS")
 
 ## Age models covaried for sex
 
@@ -120,13 +105,9 @@ run.gams.bydepth <- function(input.depth.df, output.df.name){
   gc(gam.outputs.statslist)
 }
 
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_1, output.df.name = "depth1_gamstatistics_age_sex.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_2, output.df.name = "depth2_gamstatistics_age_sex.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_3, output.df.name = "depth3_gamstatistics_age_sex.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_4, output.df.name = "depth4_gamstatistics_age_sex.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_5, output.df.name = "depth5_gamstatistics_age_sex.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_6, output.df.name = "depth6_gamstatistics_age_sex.RDS")
-run.gams.bydepth(input.depth.df = myelin.glasser.7T$depth_7, output.df.name = "depth7_gamstatistics_age_sex.RDS")
+run.gams.bydepth(input.depth.df = myelin.compartments.7T$superficial, output.df.name = "superficial_gamstatistics_age_sex.RDS")
+run.gams.bydepth(input.depth.df = myelin.compartments.7T$middle, output.df.name = "middle_gamstatistics_age_sex.RDS")
+run.gams.bydepth(input.depth.df = myelin.compartments.7T$deep, output.df.name = "deep_gamstatistics_age_sex.RDS")
 
 ############################################################################################################
 #### Fit sex effect GAMs and save out main effect statistics ####
@@ -150,13 +131,9 @@ fit.sexeffects.bydepth <- function(input.depth.df, output.df.name){
   gc(gam.sex.statistics)
 }
 
-fit.sexeffects.bydepth(input.depth.df = myelin.glasser.7T$depth_1, output.df.name = "depth1_sex_maineffects.RDS")
-fit.sexeffects.bydepth(input.depth.df = myelin.glasser.7T$depth_2, output.df.name = "depth2_sex_maineffects.RDS")
-fit.sexeffects.bydepth(input.depth.df = myelin.glasser.7T$depth_3, output.df.name = "depth3_sex_maineffects.RDS")
-fit.sexeffects.bydepth(input.depth.df = myelin.glasser.7T$depth_4, output.df.name = "depth4_sex_maineffects.RDS")
-fit.sexeffects.bydepth(input.depth.df = myelin.glasser.7T$depth_5, output.df.name = "depth5_sex_maineffects.RDS")
-fit.sexeffects.bydepth(input.depth.df = myelin.glasser.7T$depth_6, output.df.name = "depth6_sex_maineffects.RDS")
-fit.sexeffects.bydepth(input.depth.df = myelin.glasser.7T$depth_7, output.df.name = "depth7_sex_maineffects.RDS")
+fit.sexeffects.bydepth(input.depth.df = myelin.compartments.7T$superficial, output.df.name = "superficial_sex_maineffects.RDS")
+fit.sexeffects.bydepth(input.depth.df = myelin.compartments.7T$middle, output.df.name = "middle_sex_maineffects.RDS")
+fit.sexeffects.bydepth(input.depth.df = myelin.compartments.7T$deep, output.df.name = "deep_sex_maineffects.RDS")
 
 ############################################################################################################
 #### Fit age-by-sex interaction GAMs and save out interaction statistics ####
@@ -180,11 +157,7 @@ fit.agesexinteraction.bydepth <- function(input.depth.df, output.df.name){
   gc(gam.agebysex.statistics)
 }
 
-fit.agesexinteraction.bydepth(input.depth.df = myelin.glasser.7T$depth_1, output.df.name = "depth1_sex_ageinteraction.RDS")
-fit.agesexinteraction.bydepth(input.depth.df = myelin.glasser.7T$depth_2, output.df.name = "depth2_sex_ageinteraction.RDS")
-fit.agesexinteraction.bydepth(input.depth.df = myelin.glasser.7T$depth_3, output.df.name = "depth3_sex_ageinteraction.RDS")
-fit.agesexinteraction.bydepth(input.depth.df = myelin.glasser.7T$depth_4, output.df.name = "depth4_sex_ageinteraction.RDS")
-fit.agesexinteraction.bydepth(input.depth.df = myelin.glasser.7T$depth_5, output.df.name = "depth5_sex_ageinteraction.RDS")
-fit.agesexinteraction.bydepth(input.depth.df = myelin.glasser.7T$depth_6, output.df.name = "depth6_sex_ageinteraction.RDS")
-fit.agesexinteraction.bydepth(input.depth.df = myelin.glasser.7T$depth_7, output.df.name = "depth7_sex_ageinteraction.RDS")
+fit.agesexinteraction.bydepth(input.depth.df = myelin.compartments.7T$superficial, output.df.name = "superficial_sex_ageinteraction.RDS")
+fit.agesexinteraction.bydepth(input.depth.df = myelin.compartments.7T$middle, output.df.name = "middle_sex_ageinteraction.RDS")
+fit.agesexinteraction.bydepth(input.depth.df = myelin.compartments.7T$deep, output.df.name = "deep_sex_ageinteraction.RDS")
 
